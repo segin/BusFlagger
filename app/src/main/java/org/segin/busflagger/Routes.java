f1b5a18f906f37bb691a2bcfac7a60a3c038e895
Kirn Gill <segin2005@gmail.com>
2016-08-07 04:45:29 -0500

    Added skeletal code

--- app/src/main/java/org/segin/busflagger/Routes.java
+++ app/src/main/java/org/segin/busflagger/Routes.java
@@ -1,8 +1,45 @@
 package org.segin.busflagger;
 
-/**
- * Created by segin on 2016-05-09.
- */
+import android.graphics.Color;
+
 public class Routes {
-    
+    public String getRouteNumber() {
+        return routeNumber;
+    }
+
+    public void setRouteNumber(String routeNumber) {
+        this.routeNumber = routeNumber;
+    }
+
+    public String getRouteName() {
+        return routeName;
+    }
+
+    public void setRouteName(String routeName) {
+        this.routeName = routeName;
+    }
+
+    public Color getRouteColor() {
+        return routeColor;
+    }
+
+    public void setRouteColor(Color routeColor) {
+        this.routeColor = routeColor;
+    }
+
+    private String routeNumber;
+    private String routeName;
+    private Color routeColor;
+
+    public Routes() {
+
+    }
+
+    public Routes(String number, String name, Color color) {
+        routeNumber = number;
+        routeName = name;
+        routeColor = color;
+    }
+
+
 }
